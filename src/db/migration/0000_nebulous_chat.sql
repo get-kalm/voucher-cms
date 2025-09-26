c016b63e28f7892ec1d40df649874f45def8dc0e
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2025-09-26 10:52:39 +0700

    feat(db): created connection using drizzle orm for postgres

---
+++ src/db/migration/0000_nebulous_chat.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "vouchers" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "vouchers_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"is_active" boolean DEFAULT false NOT NULL,
+	"isRedeemed" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"deleted_at" timestamp with time zone DEFAULT null,
+	CONSTRAINT "vouchers_name_unique" UNIQUE("name")
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import { integer, pgTable, varchar, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const vouchersTable = pgTable("vouchers", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull().unique(),
+  isActive: boolean("is_active").notNull().default(false),
+  isRedeemed: boolean("isRedeemed").notNull().default(false),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  deletedAt: timestamp("deleted_at", { withTimezone: true })
+    .default(null),
+});
